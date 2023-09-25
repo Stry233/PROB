3cfad30b54d55330cba84129ae40edfa1e8bda25
yuetian <cheny63@rpi.edu>
2023-09-25 00:59:17 -0400

    server config align

--- configs/EASIER-M_OWOD_BENCHMARK.sh
+++ configs/EASIER-M_OWOD_BENCHMARK.sh
@@ -10,11 +10,11 @@ WANDB_NAME=PROB_V1
 
 python -u main_open_world.py \
     --output_dir "${EXP_DIR}/t1" --dataset TOWOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20\
-    --train_set 'owod_t1_train' --test_set 'owod_all_task_test' --epochs 41\
+    --train_set 'owod_t1_train' --test_set 'owod_all_task_test' --epochs 51\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
     --wandb_name "${WANDB_NAME}_t1" --exemplar_replay_selection --exemplar_replay_max_length 850\
     --exemplar_replay_dir ${WANDB_NAME} --exemplar_replay_cur_file "learned_owod_t1_ft.txt"\
-    --pretrain "${EXP_DIR}/t1.pth" --lr 2e-5\
+    --pretrain "${EXP_DIR}/t1.pth" --lr 2e-5 --training_seed 24\
     ${PY_ARGS}
     
 
