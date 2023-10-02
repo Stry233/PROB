88ba63f10691a41a7801c13cfb6d43e16f198e1b
Yuetian Chen <j.a.m_01@outlook.com>
2023-10-02 10:59:35 -0400

    update eval config

--- configs/ENSEMBLE-EVAL_M_OWOD_BENCHMARK.sh
+++ configs/ENSEMBLE-EVAL_M_OWOD_BENCHMARK.sh
@@ -14,34 +14,34 @@ python -u main_open_world.py \
     --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20 \
     --train_set "owod_t1_train" --test_set 'owod_all_task_test' --epochs 191 --lr_drop 35\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-    --pretrain "${EXP_DIR}/t1/checkpoint0037.pth" --eval --wandb_project ""\
+    --pretrain "${EXP_DIR}/t1/ckpt/24-checkpoint0040.pth" --eval --wandb_project ""\
     ${PY_ARGS}
-    
-    
+
+
 PY_ARGS=${@:1}
 python -u main_open_world.py \
-    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 20 --CUR_INTRODUCED_CLS 20 \
+    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20 \
     --train_set "owod_t1_train" --test_set 'owod_all_task_test' --epochs 191 --lr_drop 35\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-    --pretrain "${EXP_DIR}/t2.pth" --eval --wandb_project ""\
+    --pretrain "${EXP_DIR}/t1/ckpt/25-checkpoint0040.pth" --eval --wandb_project ""\
     ${PY_ARGS}
-    
-    
+
+
 PY_ARGS=${@:1}
 python -u main_open_world.py \
-    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 40 --CUR_INTRODUCED_CLS 20 \
+    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20 \
     --train_set "owod_t1_train" --test_set 'owod_all_task_test' --epochs 191 --lr_drop 35\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-    --pretrain "${EXP_DIR}/t3.pth" --eval --wandb_project ""\
+    --pretrain "${EXP_DIR}/t1/ckpt/26-checkpoint0040.pth" --eval --wandb_project ""\
     ${PY_ARGS}
-    
-    
+
+
 PY_ARGS=${@:1}
 python -u main_open_world.py \
-    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 60 --CUR_INTRODUCED_CLS 20 \
+    --output_dir "${EXP_DIR}/eval" --dataset TOWOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 20 \
     --train_set "owod_t1_train" --test_set 'owod_all_task_test' --epochs 191 --lr_drop 35\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-    --pretrain "${EXP_DIR}/t4.pth" --eval --wandb_project ""\
+    --pretrain "${EXP_DIR}/t1/ckpt/27-checkpoint0040.pth" --eval --wandb_project ""\
     ${PY_ARGS}
-    
-    
+
+
--- run_eval.sh
+++ run_eval.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/CUSTOM_EVAL_M_OWOD_BENCHMARK.sh
+GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/ENSEMBLE-EVAL_M_OWOD_BENCHMARK.sh
