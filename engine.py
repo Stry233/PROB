4b965af9cf3f680f6abf7e8ba1176f45b8bc90d5
Yuetian Chen <j.a.m_01@outlook.com>
2023-10-25 13:40:31 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -114,7 +114,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         )
  
     for samples, targets in metric_logger.log_every(data_loader, 10, header):
-        print(samples, "------", targets)
+        print("-----sample-----\n", samples, "\n---targets---\n", targets, "\n-----")
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
         outputs = model(samples)
@@ -126,7 +126,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
             target_sizes = torch.stack([t["size"] for t in targets], dim=0)
             results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
         res = {target['image_id'].item(): output for target, output in zip(targets, results)}
-        print(res)
+        print("----res-----\n", res, "\n-----")
         if coco_evaluator is not None:
             coco_evaluator.update(res)
  
