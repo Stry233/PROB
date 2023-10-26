75d0c942e328872afd88e788401189c7be3c6556
yuetian <cheny63@rpi.edu>
2023-10-26 04:11:02 -0400

    update server side

--- datasets/open_world_eval.py
+++ datasets/open_world_eval.py
@@ -322,10 +322,6 @@ def voc_eval(detpath,
     [use_07_metric]: Whether to use VOC07's 11 point AP computation
         (default False)
     """
-    print(detpath,
-             annopath,
-             imagesetfile,
-             classname)
 
     def iou(BBGT, bb):
         ixmin = np.maximum(BBGT[:, 0], bb[0])
